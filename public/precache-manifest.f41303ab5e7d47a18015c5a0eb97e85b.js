c5375ecd90f93be21ae1a4804a7faf7c66201cf1
vagrant <vagrant@homestead>
2018-11-28 15:49:40 +0000

    no laravel

---
+++ public/precache-manifest.f41303ab5e7d47a18015c5a0eb97e85b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "823597845428b27c9d87",
+    "url": "/static/js/main.82359784.chunk.js"
+  },
+  {
+    "revision": "0cd6058fc3c24877ba89",
+    "url": "/static/js/1.0cd6058f.chunk.js"
+  },
+  {
+    "revision": "823597845428b27c9d87",
+    "url": "/static/css/main.e45548e6.chunk.css"
+  },
+  {
+    "revision": "942ab00b15d071d2e62170ab57ab9611",
+    "url": "/index.html"
+  }
+];
